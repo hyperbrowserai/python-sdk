847a1935ac926db2044fef72ab4ebe520180cda8
Nikhil Shahi <nshahi1998@gmail.com>
2025-10-15 14:17:46 -0500

    add webdriver_endpoint to session detail, add claude haiku 4.5 model to claude computer use

--- hyperbrowser/models/consts.py
+++ hyperbrowser/models/consts.py
@@ -39,6 +39,7 @@ HyperAgentLlm = Literal[
     "gpt-4.1-nano",
 ]
 ClaudeComputerUseLlm = Literal[
+    "claude-haiku-4-5-20251001",
     "claude-sonnet-4-5",
     "claude-sonnet-4-20250514",
     "claude-3-7-sonnet-20250219",
